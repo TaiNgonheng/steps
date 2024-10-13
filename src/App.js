a5eb88f8e0865629e449d7bd64bba13bc587fcc3
TaiNgonheng <heng68807@gmail.com>
2024-10-13 11:01:06 +0700

    state

--- src/App.js
+++ src/App.js
@@ -9,21 +9,31 @@ const messages = [
 
 export default function App(){
    const [step, setStep]= useState(1);
+   const [test, setTest] = useState({name:""})
+   const [isOpen, setIsOpen] = useState(0)
+
   function handlePrevious(){
     if(step>1) setStep(step -1)
   } 
-  function handleNext(){
+
+  function handleNext(){ 
     if (step<3)setStep(step +1);
+    setTest({ name:"heng"})
   }
 
   return(
+    <div>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times</button>
+      {isOpen &&
     <div className="steps">
       <div className="numbers">
           <div className={step>=1 ? 'active':''}>1</div>
           <div className={step>=2 ? 'active':''}>2</div>
           <div className={step>=3 ? 'active':''}>3</div>
       </div>
-      <p className="message">step {step}: {messages[step-1]}</p>
+      <p className="message">step {step}: {messages[step-1]}
+        {test.name}
+      </p>
 
       <div className="buttons">
         <button 
@@ -37,5 +47,6 @@ export default function App(){
         >Next</button>
       </div>
     </div>
+     }</div>
   );
 }
